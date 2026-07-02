4bca407802245a93742b4a7aa804aab98d4cff69
shaslibansi <shaslibansi6@gmail.com>
2026-07-02 18:27:27 +0800

    add

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -61,7 +61,7 @@ export default function App() {
 
   return (
     <div className="min-h-screen bg-gray-100 flex">
-      <Sidebar screen={screen} setScreen={setScreen} t={t} municipality={municipality} region={region} />
+      <Sidebar screen={screen} setScreen={setScreen} t={t} />
 
       <main className="flex-1 flex flex-col min-h-screen pt-14 md:pt-0">
         {screen === "dashboard" && (
